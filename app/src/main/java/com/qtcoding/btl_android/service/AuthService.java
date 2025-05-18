7955adc8d63e706d69340dfb5b2b03758bebac75
NamLe179 <ldnam179@gmail.com>
2025-05-18 19:40:42 +0700

    update AuthService, LoginFragment

--- app/src/main/java/com/qtcoding/btl_android/service/AuthService.java
+++ app/src/main/java/com/qtcoding/btl_android/service/AuthService.java
@@ -1,5 +1,10 @@
 package com.qtcoding.btl_android.service;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -12,5 +17,24 @@ public class AuthService {
 
     public FirebaseUser getCurrentUser() {
         return firebaseAuth.getCurrentUser();
+    }
+
+    public void login(String email, String password, ServiceCallback<Void> callback){
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    callback.onSuccess(null);
+                }
+                else {
+                    callback.onFailure(task.getException());
+                }
+            }
+        });
+    }
+
+    public void logout(ServiceCallback<Void> callback){
+        firebaseAuth.signOut();
+        callback.onSuccess(null);
     }
 }
